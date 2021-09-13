6e380eeddb3dd3cff8747c518dcf05fc6a77e8c8
jorsuap <jorsuap@gmail.com>
2021-09-13 17:48:10 -0500

    master

--- js/carritocompras.js
+++ js/carritocompras.js
@@ -4,7 +4,26 @@ const carrito = document.querySelector('#carrito');
 const listaProductos = document.querySelectorAll('.comprar');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');
+let cantidadProductos = document.querySelector('.incador-cantidad');
+let total = document.querySelector('.totalPagar');
+let totalmsn = document.querySelector('.total');
+const msnvacio = document.querySelector('.msnvacio');
+let cantidadTotal = 0;
+let totalPagar = 0;
 let articulosCarrito = [];
+
+function carritoVacio(){
+    if(articulosCarrito.length < 1){
+        vaciarCarrito.classList.add('hiddecarrito');
+        totalmsn.classList.add('hiddecarrito');
+        msnvacio.classList.add('msnvacio-block');
+    }else{
+        vaciarCarrito.classList.remove('hiddecarrito');
+        totalmsn.classList.remove('hiddecarrito');
+        msnvacio.classList.remove('msnvacio-block');
+    }
+}
+carritoVacio();
 
 //agregamos un Listenner a cada boton COMPRAR y ejecutamos funcion agregar producto
 listaProductos.forEach(function (comprar) {
@@ -12,11 +31,11 @@ listaProductos.forEach(function (comprar) {
         e.preventDefault();
         const productoSelecionado = e.target.parentElement.parentElement;
         agregarProducto(productoSelecionado);
-        console.log(e.target);
     })
 });
 
 cargarEvenlisteners();
+
 function cargarEvenlisteners() {
     //elimina cursos del carrito
     carrito.addEventListener('click', eliminarproducto);
@@ -24,17 +43,30 @@ function cargarEvenlisteners() {
 
 //Funcion eliminar curso
 function eliminarproducto(e) {
-    
-    if(e.target.classList.contains('borrar-producto')){
+
+    if (e.target.classList.contains('borrar-producto')) {
+
         const productoId = e.target.getAttribute('data-id');
-        //elliminar del arreglo
+
+        //esta funcion elimina la cantidad de unidades de un producto en el contador
+        articulosCarrito.map(producto => {
+            if (producto.id === productoId) {
+                cantidadTotal -= producto.cantidad;
+                totalPagar -= parseInt(producto.precio); //Total a pagar bill
+                totalaPagar();
+                console.log(totalPagar);
+            }
+        })
+        //elliminar del arreglo filtrando por ID
         articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
         mostrarenHTML();
+        actualizardisplay();
+        carritoVacio();
     }
 }
 
-
 //Funcion crear un objeto con la informacion del curso y luego lo agrega a un arreglo
+
 function agregarProducto(producto) {
     const infoProducto = {
         imagen: producto.querySelector('.item').src,
@@ -43,6 +75,12 @@ function agregarProducto(producto) {
         id: producto.querySelector('button').getAttribute('data-id'),
         cantidad: 1
     }
+
+    //SUMANDO LA CANTIDAD TOTAL DE PRODUCTOS EN EL CARRITO
+
+    cantidadTotal += infoProducto.cantidad;
+    totalPagar += parseInt(infoProducto.precio); //Total a pagar bill
+    totalaPagar();
 
     //revisar si ya existe un cursor: 
     const existe = articulosCarrito.some(producto => producto.id === infoProducto.id);
@@ -55,6 +93,7 @@ function agregarProducto(producto) {
                 infoProducto.precio = parseInt(infoProducto.precio);
                 precioInicial = infoProducto.precio;
                 producto.precio += precioInicial;
+
                 return producto;
             } else {
                 return producto;
@@ -65,6 +104,8 @@ function agregarProducto(producto) {
         articulosCarrito = [...articulosCarrito, infoProducto]
     }
     mostrarenHTML();
+    actualizardisplay();
+    carritoVacio();
 };
 
 //Funcion para mostrar los productos en el Carrito de compras
@@ -79,34 +120,49 @@ function mostrarenHTML() {
             cantidad
         } = producto;
         const row = document.createElement('tr');
-        row.innerHTML =`
+        row.innerHTML = `
             <td>
-                <img src ="${imagen}" width="100">
+                <img src ="${imagen}" width="100px">
             </td>
-            <td>
-                ${name}
-            </td>
-            <td>
-                ${cantidad}
-            </td>
-            <td>
-                $${precio}
+            <td class="alinear" >
+                <div class="name-producto-carrito">${name}</div>
+                <div>$${precio} (${cantidad})</div>
             </td>
             <td>
                 <button class="borrar-producto" data-id="${producto.id}">X</button>
             </td>
             `;
-            contenedorCarrito.appendChild(row);
+        contenedorCarrito.appendChild(row);
     })
 };
+
 function limpiarHTML() {
     // contenedorCarrito.innerHTML ='';
     while (contenedorCarrito.firstChild) { //De esta forma se elimina la redundancia mas rapido
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
-    }
+    }    
 };
 vaciarCarrito.addEventListener('click', () => {
     articulosCarrito = [];
+    cantidadTotal = 0;
+    totalPagar = 0;
+    actualizardisplay();
     limpiarHTML();
+    totalaPagar();
+    carritoVacio();
+    
 });
+
+function actualizardisplay() {
+    if (articulosCarrito.length >= 1) {
+        cantidadProductos.innerHTML = cantidadTotal;
+        cantidadProductos.classList.add('incador-cantidad_visible');
+    } else {
+        cantidadProductos.classList.remove('incador-cantidad_visible');
+    };
+};
+
+function totalaPagar() {
+    total.innerHTML = totalPagar;
+}
 
